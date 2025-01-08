bc936264825f0f6a21a5f39c45f4688a92db6c26
varshitha <144579447+varshitha229@users.noreply.github.com>
2025-01-08 14:10:46 +0530

    Add files via upload

---
+++ gas.ino/gas.ino.ino
@@ -0,0 +1,12 @@
+// #define gas A0
+// void setup() {
+//   Serial.begin(9600);
+//   pinMode(gas,INPUT);
+
+// }
+
+// void loop() {
+//   int val = analogRead(gas);
+//   Serial.println(val);
+//   delay(1000);
+// }
